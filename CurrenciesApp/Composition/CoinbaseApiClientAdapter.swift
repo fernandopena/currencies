895db89affa5c7465bbdd3a161c5d3e793e16795
penafernando <fernando.pena@pomelo.la>
2022-10-31 10:48:28 -0300

    Create `CoindeskApiClientUIAdapter`

--- CurrenciesApp/Composition/CoinbaseApiClientAdapter.swift
+++ CurrenciesApp/Composition/CoinbaseApiClientAdapter.swift
@@ -20,3 +20,30 @@ extension CoindeskApiClient: CurrencyViewDataSourceProtocol {
         }
     }
 }
+
+class CoindeskApiClientUIAdapter: CurrencyViewDataSourceProtocol {
+    let apiClient = CoindeskApiClient()
+    
+    func fetchCurrenciesViewData(completionHandler: @escaping (Result<[CurrencyViewData], Error>) -> Void) {
+        apiClient.fetchCurrencies { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let dto):
+                let currencies = self.map(dto: dto)
+                completionHandler(.success(currencies))
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        }
+    }
+    
+    func map(dto: CoindeskResponseDTO) -> [CurrencyViewData] {
+        dto.data.map { CurrencyViewData(coin: $1) }
+    }
+}
+
+extension CurrencyViewData {
+    init(coin: CoindeskResponseDTO.Coin) {
+        self = CurrencyViewData(name: coin.name, rate: "USD: \(coin.rate)")
+    }
+}
